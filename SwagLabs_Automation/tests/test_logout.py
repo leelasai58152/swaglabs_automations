64b8d83e0f89f8f42536c33c4b95ca8fc73c637b
leelasai58152 <yanamalaleelasai8@gmail.com>
2026-07-17 16:13:40 +0530

    Fixed logout test

--- SwagLabs_Automation/tests/test_logout.py
+++ SwagLabs_Automation/tests/test_logout.py
@@ -1,3 +1,6 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from pages.login_page import LoginPage
 from pages.menu_page import MenuPage
 from pages.logout_page import LogoutPage
@@ -18,5 +21,9 @@ def test_logout(setup):
 
     logout = LogoutPage(driver)
     logout.logout()
+
+    WebDriverWait(driver, 10).until(
+        EC.url_to_be(URL)
+    )
 
     assert driver.current_url == URL
